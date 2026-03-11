c11fe9cbbe2083ba2dc18a1b9301bd504ccf5d0b
Apocrypher00 <coltonkenney@hotmail.com>
2026-03-10 22:35:30 -0700

    Re-factored the root module, and added a dependency

--- SolarWinds.WebHelpDesk.psm1
+++ SolarWinds.WebHelpDesk.psm1
@@ -1,11 +1,9 @@
-# Module state
-$Script:WHDConnection = [PSCustomObject]@{
-    UriBuilder = $null
-    WebSession = $null
-    Session    = $null
-    # This will hold the query parameters we use for authentication, either sessionKey or apiKey/username
-    AuthParams = [System.Web.HttpUtility]::ParseQueryString([string]::Empty)
-}
+# External Imports
+Add-Type -AssemblyName "System.Web" -ErrorAction Stop
+
+# Import all enums in the Enums folder
+$Enums = Join-Path -Path $PSScriptRoot -ChildPath 'Enums'
+Get-ChildItem -Path $Enums -Filter '*.ps1' -File | ForEach-Object { . $_.FullName }
 
 # Import all functions in the Private and Public folders
 $Functions        = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
@@ -14,9 +12,13 @@ $PublicFunctions  = Join-Path -Path $Functions -ChildPath 'Public'
 Get-ChildItem -Path $PrivateFunctions -Filter '*.ps1' -File | ForEach-Object { . $_.FullName }
 Get-ChildItem -Path $PublicFunctions  -Filter '*.ps1' -File | ForEach-Object { . $_.FullName }
 
-# Import all enums in the Enums folder
-$Enums = Join-Path -Path $PSScriptRoot -ChildPath 'Enums'
-Get-ChildItem -Path $Enums -Filter '*.ps1' -File | ForEach-Object { . $_.FullName }
+# Module State
+$Script:WHDConnection = [PSCustomObject]@{
+    UriBuilder = $null
+    WebSession = $null
+    Session    = $null
+    AuthParams = [System.Web.HttpUtility]::ParseQueryString([string]::Empty)
+}
 
 # Register type accelerators for enums?
 # $accelerators = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")
