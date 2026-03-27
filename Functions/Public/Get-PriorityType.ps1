601887b227c1a1efc7ec17aece838c328b5d940b
Apocrypher00 <coltonkenney@hotmail.com>
2026-03-27 16:07:22 -0700

    Added PriorityTypes

---
+++ Functions/Public/Get-PriorityType.ps1
@@ -0,0 +1,37 @@
+<#
+    .SYNOPSIS
+    Get a PriorityType from WHD.
+
+    .DESCRIPTION
+    This function retrieves a specific PriorityType from WHD, or a list of all PriorityTypes.
+
+    .PARAMETER ResourceId
+    The id of the PriorityType to be retrieved.
+
+    .PARAMETER Expand
+    If specified, all results will be in the detailed format.
+
+    .NOTES
+    This ResourceType doesn't support Qualifiers.
+#>
+function Get-PriorityType {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [int] $ResourceId,
+
+        [Parameter()]
+        [switch] $Expand
+    )
+
+    $QueryParameters = @{
+        ResourceType = [WHDResourceType]::PriorityTypes
+        Expand       = $Expand.IsPresent
+    }
+
+    if ($PSBoundParameters.ContainsKey("ResourceId")) {
+        $QueryParameters["ResourceId"] = $ResourceId
+    }
+
+    return Get-Resource @QueryParameters
+}
